689a8f67bdfb3532c4ef8524898a5e7e697a4a94
Ahmad Ahmad Ibrahim <ahmadibrahimahmad212@gmail.com>
2026-04-15 16:14:00 +0100

    Create App.js for React main component for CV-JD alignment

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CV-JD Alignment</h1>
+      <p>This application will help users align their CV with job descriptions.</p>
+    </div>
+  );
+};
+
+export default App;
